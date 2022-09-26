4b04eb3cf936e86e87348d3e89331e9dd9ed23b6
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-09-26 13:25:34 -0700

    Add project files.

---
+++ CupsAndCakes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CupsAndCakes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
