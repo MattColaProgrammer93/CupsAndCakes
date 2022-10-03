1987c871679809c9f8c5f1a4b5134aa9fb194746
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-10-03 15:17:49 -0700

    Created the default roles

--- CupsAndCakes/Controllers/HomeController.cs
+++ CupsAndCakes/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CupsAndCakes.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace CupsAndCakes.Controllers
             return View();
         }
 
+        [Authorize(Roles = IdentityHelper.Customer)]
         public IActionResult Privacy()
         {
             return View();
