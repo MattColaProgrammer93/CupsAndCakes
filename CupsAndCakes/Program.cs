62346c8d49fbe8da2852ec412567dab93a828b94
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-10-17 14:17:38 -0700

    Have the program to create customer repo objects

--- CupsAndCakes/Program.cs
+++ CupsAndCakes/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
