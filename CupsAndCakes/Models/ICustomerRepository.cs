182ef63e7a57a263c127397a48870f073215987e
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-10-17 13:11:54 -0700

    Added class and methods

--- CupsAndCakes/Models/ICustomerRepository.cs
+++ CupsAndCakes/Models/ICustomerRepository.cs
@@ -1,7 +1,50 @@
-﻿namespace CupsAndCakes.Models
+﻿using CupsAndCakes.Data;
+
+namespace CupsAndCakes.Models
 {
     public interface ICustomerRepository
     {
-        
+        void SaveCustomer(Customer customer);
+        IEnumerable<Customer> GetAllCustomers();
+        void DeleteCustomer(int customerId);
+        void UpdateCustomer(Customer customer);
+        Customer GetCustomer(int customerId);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context )
+        {
+            _context = context;
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            Customer customer = GetCustomer(customerId);
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int customerId)
+        {
+            return _context.Customers.SingleOrDefault(i => i.Id == customerId);
+        }
+
+        public void SaveCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
