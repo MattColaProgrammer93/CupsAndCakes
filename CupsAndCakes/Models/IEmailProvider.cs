4205ef41cab0b0f5d287be2044eeb14aa6f6dbaf
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-11-11 11:42:25 -0800

    Created the IEmailProvider along with EmailProviderSendGrid class

---
+++ CupsAndCakes/Models/IEmailProvider.cs
@@ -0,0 +1,16 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace CupsAndCakes.Models
+{
+    public interface IEmailProvider
+    {
+        Task SendEmailAsync(string toEmail, string fromEmail, string subject,
+                                string content, string htmlContent);
+    }
+
+    public class EmailProviderSendGrid : IEmailProvider
+    {
+        
+    }
+}
