b3b01f5d3d42f753959dbe496234df068cc6be7a
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-11-11 11:57:32 -0800

    Added code to the interface, and added code to the EmailProviderSendGrid where it sends a email

--- CupsAndCakes/Models/IEmailProvider.cs
+++ CupsAndCakes/Models/IEmailProvider.cs
@@ -11,6 +11,27 @@ namespace CupsAndCakes.Models
 
     public class EmailProviderSendGrid : IEmailProvider
     {
-        
+        private readonly IConfiguration _config;
+
+        public EmailProviderSendGrid(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string fromEmail, string subject, string content, string htmlContent)
+        {
+            var apiKey = _config.GetSection("SendGridKey").Value;
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_config.GetSection("FromEmail").Value, "CupsAndCakes"),
+                Subject = "Testing",
+                PlainTextContent = "This is a test email",
+                HtmlContent = "<strong>This is a test email</strong>"
+            };
+            msg.AddTo(new EmailAddress("mattchoque115@gmail.com", "Matthew Choque"));
+            await client.SendEmailAsync(msg);
+            // var response = await client SendEmailAsync(msg).ConfigureAwait(false);
+        }
     }
 }
