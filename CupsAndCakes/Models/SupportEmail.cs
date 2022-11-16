7e52be55d109334eb7a39f90cfd5ef3872b47dcf
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-11-16 09:04:28 -0800

    Created SupportEmail class

---
+++ CupsAndCakes/Models/SupportEmail.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CupsAndCakes.Models
+{
+    /// <summary>
+    /// A email that will be sent to the support team of CupsAndCakes
+    /// </summary>
+    public class SupportEmail
+    {
+        /// <summary>
+        /// The unique indentifier for each email id
+        /// </summary>
+        [Key]
+        public int SupportId { get; set; }
+
+        /// <summary>
+        /// The subject of the email
+        /// </summary>
+        [Required]
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// The content of the email
+        /// </summary>
+        [Required]
+        public string Content { get; set; }
+    }
+}
